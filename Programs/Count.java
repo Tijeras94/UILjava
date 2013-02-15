3cf4bdb4e34b9782770edd2c98f3f443c99a2d89
maufer94 <mamezcua94@gmail.com>
2013-02-14 20:33:27 -0600

    update

--- Programs/Count.java
+++ Programs/Count.java
@@ -2,25 +2,25 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-
 public class Count 
 {
 	public static void main(String args[]) throws IOException
 	{
 		Scanner kb = new Scanner (new File("count.dat"));
-		
+
 		int times = Integer.parseInt(kb.nextLine().trim());
-		
+
 		for (int i = 0; i < times; i++)
 		{
 			String temp = kb.nextLine().trim();
-			
+
 			int n = Integer.parseInt(temp.substring(0,2));
 			int r = Integer.parseInt(temp.substring(3));
-			
+
 			char methd = temp.charAt(2);
-			
+
 			switch (methd)
+			{
 				case 'C':
 					{
 						System.out.println(temp + " This is Combination.");
@@ -29,6 +29,7 @@ public class Count
 					{
 						System.out.println(temp + " This is Permutation.");
 					}
+			}
 		}
 		kb.close();
 	}
