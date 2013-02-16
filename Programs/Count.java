5596662ca2195660ff638b5712d343267d001b1a
maufer94 <mamezcua94@gmail.com>
2013-02-15 19:09:28 -0600

    set class variables

--- Programs/Count.java
+++ Programs/Count.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class Count 
-{	
+{
+	public static long permutation, combination;
+	
 	public static long factorial(long n)
 	{
 		if (n == 1)
@@ -15,12 +17,14 @@ public class Count
 	
 	public static long permutation(long n, long nr)
 	{
-		return n / (nr); 
+		permutation = n / (nr);
+		return permutation; 
 	}
 	
 	public static long combination(long n, long r, long nr)
 	{
-		return n / (r*(nr));
+		combination = n / (r*(nr));
+		return combination;
 	}
 	
 	public static void main(String args[]) throws IOException
@@ -32,13 +36,10 @@ public class Count
 		for (int i = 0; i < times; i++)
 		{
 			String temp = kb.nextLine().trim();
-
-			//long n = Long.parseLong(temp.substring(0,2));
-			//long r = Long.parseLong(temp.substring(3));
 			
 			char methd = temp.charAt(2);
 			
-	switch (methd)
+			switch (methd)
 			{
 				case 'C':
 					{
